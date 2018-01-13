f42a243d4ce2109bbd7a4b3bcecf2d01129bc5ce
DillionApple <DillionMango@gmail.com>
2018-01-13 18:29:21 +0800

    add logout function, improve robust

--- fdu_network_auto_auth.py
+++ fdu_network_auto_auth.py
@@ -9,23 +9,49 @@ from account_config import *
 
 url = r'http://10.108.255.249/include/auth_action.php'
 
-payload = {
-    'action': 'login',
-    'username': username,
-    'password': password,
-    'ac_id': 1,
-    'ajax': 1,
-}
-
 random.seed(time.time())
+
+def do_login():
+
+    print("Do Login")
+
+    payload = {
+        'action': 'login',
+        'username': username,
+        'password': password,
+        'ac_id': 1,
+        'ajax': 1 }
+    
+    response = requests.post(url, data=payload)
+    response_str = response.content.decode('utf-8')
+
+    if (response_str.find("login_ok") == -1):
+        return False
+    return True
+
+def do_logout():
+
+    print("Do Logout")
+
+    payload = {
+        'action': 'logout',
+        'username': '',
+        'ajax': 1}
+    
+    response = requests.post(url, data=payload)
 
 def start_making_request():
 
     while True:
-        response = requests.post(url, data=payload)
         datetime_now = datetime.datetime.now()
-
-        print("{time}: {response}".format(time=datetime_now, response=response.content.decode('utf-8')))
+        if do_login():
+            print("{time}: Login Success".format(time=datetime_now))
+        else:
+            do_logout()
+            if do_login():
+                print("{time}: Login Success".format(time=datetime_now))
+            else:                
+                print("{time}: Login Fail".format(time=datetime_now))
         
         # sleep from 10 mins to 60 mins
         sleep_seconds = random.randrange(600, 3600)
@@ -33,5 +59,6 @@ def start_making_request():
         print("Next request will at {time} in {seconds} seconds".format(time=datetime_now+interval, seconds=sleep_seconds))
 
         time.sleep(sleep_seconds)
-    
-threading.Thread(target=start_making_request).start()
+
+if __name__ == '__main__':
+    threading.Thread(target=start_making_request).start()
